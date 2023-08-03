fefcd24f9f8d0b0c2a502b8e1b298cb5299f0202
eauria12 <85134383+eauria12@users.noreply.github.com>
2023-08-03 18:23:25 -0500

    Consolidado Change

--- Cliente/src/app/pages/ui-components/consolidado/consolidado.component.ts
+++ Cliente/src/app/pages/ui-components/consolidado/consolidado.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-consolidado',
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./consolidado.component.scss']
 })
 export class ConsolidadoComponent {
+
+
 
 }
