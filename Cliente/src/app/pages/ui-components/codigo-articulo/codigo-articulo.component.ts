2b2d1cb593fcc62a142e7d21a6882b2612c3563f
eauria12 <85134383+eauria12@users.noreply.github.com>
2023-08-05 11:45:45 -0500

    Changes Saldo_Inventario

--- Cliente/src/app/pages/ui-components/codigo-articulo/codigo-articulo.component.ts
+++ Cliente/src/app/pages/ui-components/codigo-articulo/codigo-articulo.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter, Input, ViewChild, SimpleChanges, ElementRef } from '@angular/core';
+import { ObtenerListasService } from 'src/app/services/obtener-listas.service';
+import { ObtenerPermisosService } from 'src/app/services/obtener-permisos.service';
+import { Linea } from 'src/app/interfaces/linea'
+
 
 @Component({
   selector: 'app-codigo-articulo',
@@ -6,5 +10,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./codigo-articulo.component.scss']
 })
 export class CodigoArticuloComponent {
+  protected lineas: Linea[] = [];
+  codigoDesde: string = '';
+  codigoHasta: string = '';
+
+  @Output() codigosEscogidos = new EventEmitter<number[]>();
+  @Input() lineasDisponibles: String;
+  @Input() disabled: boolean;
+  @ViewChild('codigoDesdeInput', { static: false }) codigoDesdeInput!: ElementRef;
+  @ViewChild('codigoHastaInput', { static: false }) codigoHastaInput!: ElementRef;
+
+  constructor(private servicioListas: ObtenerListasService, private servicioPermisos: ObtenerPermisosService) {
+  }
+
+  async ngOnChanges(changes: SimpleChanges) {
+    if (changes['disabled'] && this.disabled == true) {
+      this.codigoDesde = '';
+      this.codigoHasta = '';
+    }
+  }
+
+  async ngOnInit() {
+    if (this.lineasDisponibles.length > 1) {
+      this.lineas = await this.servicioListas.getListaPorRangoLineasSafe(this.lineasDisponibles);
+    } else {
+      this.lineas = await this.servicioListas.getListaLineasSafe();
+    }
+    console.log(this.lineas);
+  }
+
+  onInputChange() {
+    console.log(parseInt(this.codigoDesdeInput.nativeElement.value))
+    console.log(parseInt(this.codigoHastaInput.nativeElement.value))
+    if (parseInt(this.codigoDesdeInput.nativeElement.value) <= parseInt(this.codigoHastaInput.nativeElement.value)) {
+      this.codigosEscogidos.emit([parseInt(this.codigoDesdeInput.nativeElement.value), parseInt(this.codigoHastaInput.nativeElement.value )]);
+    }
+  }
+
 
 }
